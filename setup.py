f57fa452110572f10f0e9af7f8f31084ee593ea5
nklapste <nklapste@ualberta.ca>
2017-08-21 19:22:43 -0400

    major updates on setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,40 @@
-from setuptools import setup
+""" card-py-bot setup """
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
 
 
 setup(
     name="card-py-bot",
     author="Nathan Klapstein",
     author_email="nklapste@ualberta.ca",
-
-    version="3.0.4",
-
-    description="A Discord Bot for parsing magic card links",
+    description="A Discord Bot for parsing WOTC Magic card links",
+    long_description=readme(),
+    version="3.0.5", # new tag
     url="https://github.com/nklapste/card-py-bot",
     download_url="https://github.com/nklapste/card-py-bot/archive/1.4.tar.gz",
-    packages=["card_py_bot"],
+    classifiers=[
+        "License :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(exclude=["tests"]),
+    license="MIT",
     package_data={
-        '': ['README.md'],
-        'card_py_bot': ['mana_config.txt', 'MANA_ICONS/*.gif'],
+        "": ["README.md"],
+        "card_py_bot": ["mana_config.txt", "MANA_ICONS/*.gif"],
     },
     install_requires=[
-        'beautifulsoup4',
-        'html5lib',
-        'discord.py',
+        "beautifulsoup4",
+        "html5lib",
+        "discord.py"
     ],
     entry_points={
-        'console_scripts': ['card-py-bot = card_py_bot.card_bot:main'],
+        "console_scripts": ["card-py-bot = card_py_bot.__main__:main"],
     },
 )
