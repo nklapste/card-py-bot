79c86e530fb3fb499549094eea58b8621a9fa888
nklapste <nklapste@ualberta.ca>
2017-09-11 22:55:13 -0600

    improved formatting

--- card_py_bot/config_emoji.py
+++ card_py_bot/config_emoji.py
@@ -51,9 +51,9 @@ norm_id_list = [
 def print_ids():
     """Return a string of all the mana ids (in order) for config setup
     in discord"""
-    config_string = ("RECOPY THIS AND PASTE THIS INTO CHAT THEN TAKE THE" +
-                     "OUTPUT AND SAVE IT INTO CONFIG.TXT\n")
-    config_string = "?save_setup\n"
+    config_string = "RECOPY THIS AND PASTE THIS INTO CHAT THEN TAKE THE" \
+                    "OUTPUT AND SAVE IT INTO CONFIG.TXT\n"
+    config_string += "?save_setup\n"
     for raw_id in norm_id_list:
         config_string += ("\\\\" + raw_id + "\n")
 
