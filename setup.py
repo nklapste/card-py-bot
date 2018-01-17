f6b2b2a069e13270377cdbcd5471c075d0362fb9
Nathan <nklapste@ualberta.ca>
2018-01-16 21:11:37 -0500

    fixing for pypi release

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def readme():
 
 setup(
     name="card-py-bot",
-    version="3.1.2",
+    version="3.1.3",
     description="A Discord Bot for embedding WOTC Magic card links "
                 "into Discord",
     long_description=readme(),
     author="Nathan Klapstein",
     author_email="nklapste@ualberta.ca",
     url="https://github.com/nklapste/card-py-bot",
-    download_url="https://github.com/nklapste/card-py-bot/archive/3.1.2.tar.gz",
+    download_url="https://github.com/nklapste/card-py-bot/archive/3.1.3.tar.gz",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
